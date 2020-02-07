8221c70fbf52220b8269baf01458ba247c696acb
Diego Sampaio <chinello@gmail.com>
2020-02-07 15:00:57 -0300

    Update app definitions

--- src/PollCommand.ts
+++ src/PollCommand.ts
@@ -21,59 +21,5 @@ export class PollCommand implements ISlashCommand {
 
             await modify.getUiController().openModalView(modal, { triggerId }, context.getSender());
         }
-    //     console.log(context);
-    //
-    //     const room = context.getRoom();
-    //
-    //     const builder = modify.getCreator().startMessage()
-    //         .setSender(context.getSender())
-    //         .setRoom(context.getRoom())
-    //         // .setText('Choose an action')
-    //         .setAvatarUrl(avatarURL)
-    //         .setUsernameAlias('Poll');
-    //
-    //     const block = modify.getCreator().getBlockBuilder();
-    //
-    //     block.addSectionBlock({
-    //         text: {
-    //             type: TextObjectType.PLAINTEXT,
-    //             text: 'Choose an action',
-    //         },
-    //     });
-    //
-    //     block.addActionsBlock({
-    //         elements: [
-    //             {
-    //                 type: BlockElementType.BUTTON,
-    //                 text: {
-    //                     type: TextObjectType.PLAINTEXT,
-    //                     text: 'Create poll',
-    //                 },
-    //                 actionId: 'create',
-    //                 value: room.id,
-    //             } as IButtonElement,
-    //             // {
-    //             //     type: BlockElementType.BUTTON,
-    //             //     text: {
-    //             //         type: TextObjectType.PLAINTEXT,
-    //             //         text: 'Show results',
-    //             //     },
-    //             //     actionId: 'result',
-    //             //     value: 'show',
-    //             // } as IButtonElement,
-    //         ],
-    //     });
-    //
-    //     builder.setBlocks(block);
-    //
-    //     modify.getNotifier().notifyUser(context.getSender(), builder.getMessage());
-    }
-
-    private UUID() {
-        return 'xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx'.replace(/[xy]/g, (c) => {
-            const r = Math.random() * 16 | 0;
-            const v = c === 'x' ? r : (r & 0x3 | 0x8);
-            return v.toString(16);
-        });
     }
 }
